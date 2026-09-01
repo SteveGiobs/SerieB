e79edfc3c69c1a47012e6d38cbebd0610a23ecff
Giovanni Nani <62328644+SteveGiobs@users.noreply.github.com>
2026-09-01 17:54:34 +0200

    Update classifica.js

--- classifica.js
+++ classifica.js
@@ -49,7 +49,7 @@ function aggiungiSquadra(nome) {
   }
 }
 
-fetch('https://SteveGiobs.github.io/miosito/partiteClassifica.json')
+fetch('https://SteveGiobs.github.io/SerieB/partiteClassifica.json')
   .then(res => res.json())
   .then(partiteCalendario => {
     partiteCalendario.forEach(p => {
